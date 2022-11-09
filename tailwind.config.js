f4260185accdd77fa3278f26f0b8da107e45a0e5
JoshuaFaaui <joshuafaaui@gmail.com>
2022-11-09 17:32:16 +1300

    front end tailwind css attempt

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        red: "#8D0C0C",
+      },
+      backgroundImage: {
+        foundationimage:
+          "url('https://images.unsplash.com/photo-1540957904852-c2184978df5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')",
+      },
+    },
+  },
+  plugins: [],
+};
