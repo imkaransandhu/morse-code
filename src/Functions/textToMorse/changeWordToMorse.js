dc7a0b32a3a36388a305ca89ad0e86d204950121
Karan Singh <iampatwari@gmail.com>
2022-11-17 11:08:12 +1300

    UPD: Added capital letter in switch statement

--- src/Functions/textToMorse/changeWordToMorse.js
+++ src/Functions/textToMorse/changeWordToMorse.js
@@ -3,6 +3,84 @@ function changeWordToMorse(letter, morseArray) {
     case " ":
       morseArray.push("/ ");
       break;
+    case "A":
+      morseArray.push(".- ");
+      break;
+    case "B":
+      morseArray.push("-... ");
+      break;
+    case "C":
+      morseArray.push("-.-. ");
+      break;
+    case "D":
+      morseArray.push("-.. ");
+      break;
+    case "E":
+      morseArray.push(". ");
+      break;
+    case "F":
+      morseArray.push("..-. ");
+      break;
+    case "G":
+      morseArray.push("--. ");
+      break;
+    case "H":
+      morseArray.push(".... ");
+      break;
+    case "I":
+      morseArray.push(".. ");
+      break;
+    case "J":
+      morseArray.push(".--- ");
+      break;
+    case "K":
+      morseArray.push("-.- ");
+      break;
+    case "L":
+      morseArray.push(".-.. ");
+      break;
+    case "M":
+      morseArray.push("-- ");
+      break;
+    case "N":
+      morseArray.push("-. ");
+      break;
+    case "O":
+      morseArray.push("--- ");
+      break;
+    case "P":
+      morseArray.push(".--. ");
+      break;
+    case "Q":
+      morseArray.push("--.- ");
+      break;
+    case "R":
+      morseArray.push(".-. ");
+      break;
+    case "S":
+      morseArray.push("... ");
+      break;
+    case "T":
+      morseArray.push("- ");
+      break;
+    case "U":
+      morseArray.push("..- ");
+      break;
+    case "V":
+      morseArray.push("...- ");
+      break;
+    case "W":
+      morseArray.push(".-- ");
+      break;
+    case "X":
+      morseArray.push("-..- ");
+      break;
+    case "Y":
+      morseArray.push("-.-- ");
+      break;
+    case "Z":
+      morseArray.push("--.. ");
+      break;
     case "a":
       morseArray.push(".- ");
       break;
